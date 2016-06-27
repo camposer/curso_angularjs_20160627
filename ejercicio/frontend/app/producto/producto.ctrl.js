0270055e512b7cb10a9142b49f62c344cf5f823c
Rodolfo Campos <camposer@gmail.com>
2016-06-27 16:31:46 +0200

    Implementación parcial del código de producto

--- ejercicio/frontend/app/producto/producto.ctrl.js
+++ ejercicio/frontend/app/producto/producto.ctrl.js
@@ -6,6 +6,8 @@
 		.controller('ProductoCtrl', [ '$scope', ProductoCtrl ]);
 
 	function ProductoCtrl($scope) {
+		var modificar = false;
+
 		$scope.productos = [
 			{
 				nombre: 'TV',
@@ -20,6 +22,39 @@
 				precio: 350
 			}
 		];
+
+		$scope.guardar = function(form) {
+			$scope.mensajes = {
+				error: [],
+				success: []
+			};
+
+			if (form.nombre.$invalid)
+				$scope.mensajes.error.push( 'Nombre inválido');
+			if (form.precio.$invalid ||
+				$scope.producto.precio <= 0)
+				$scope.mensajes.error.push('Precio inválido');
+
+			if ($scope.mensajes.error.length == 0) {
+				if (modificar)
+					$scope.$apply();
+				else
+					$scope.productos.push(producto);
+
+				$scope.mensajes.success.push('Producto agregado');
+				$scope.producto = {};
+			}
+		};
+	
+		$scope.eliminar = function(p) {
+			var idx = $scope.productos.indexOf(p);
+			$scope.productos.splice(idx, 1);
+		};
+
+		$scope.mostrar = function(p) {
+			$scope.producto = p;
+			modificar = true;
+		}
 	}
 
 })();
